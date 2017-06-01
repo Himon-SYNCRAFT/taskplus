33597abb9fb08dc6a0e9a1e954e9c050d6613347
Himon-SYNCRAFT <d.zawlocki@syncraft.pl>
2017-06-01 22:48:05 +0200

    Fix conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,6 @@ from taskplus.apps.rest.app import create_app
 from taskplus.apps.rest.settings import TestConfig
 
 
-@pytest.yield_fixture(scope='function')
+@pytest.fixture(scope='function')
 def app():
     return create_app(TestConfig)
