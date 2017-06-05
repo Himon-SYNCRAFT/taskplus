2a11c8341f18e1c6fc733ef9dab0261b36d3c3ee
Himon-SYNCRAFT <d.zawlocki@syncraft.pl>
2017-06-06 00:23:46 +0200

    Fixing mistake. Undo delete auth.py

---
+++ taskplus/apps/rest/auth.py
@@ -0,0 +1,53 @@
+from flask_login import LoginManager
+
+from taskplus.core.actions import ListUsersAction, ListUsersRequest
+from taskplus.apps.rest.repositories import UsersRepository
+
+
+login_manager = LoginManager()
+
+
+@login_manager.user_loader
+def user_loader(name):
+    repository = UsersRepository()
+    request = ListUsersRequest(filters=dict(name=name))
+    action = ListUsersAction(repo=repository)
+    response = action.execute(request)
+
+    if not response or not response.value:
+        return None
+
+    user = response.value[0]
+    user.is_active = True
+    user.is_authenticated = True
+    user.is_anonymous = False
+    user.get_id = lambda _: user.name
+
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    name = request.form.get('name')
+
+    repository = UsersRepository()
+    request = ListUsersRequest(filters=dict(name=name))
+    action = ListUsersAction(repo=repository)
+    response = action.execute(request)
+
+    if not response or not response.value:
+        return None
+
+    user = response.value[0]
+    user.is_authenticated = repository.check_password(
+        user, request.form['password'])
+    user.is_active = True
+    user.is_anonymous = False
+    user.get_id = lambda _: user.name
+
+    return user
+
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return 'Unauthorized', 401
