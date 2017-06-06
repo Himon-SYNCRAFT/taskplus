f708a4555537720780d9fc7e0099638355759940
Himon-SYNCRAFT <d.zawlocki@syncraft.pl>
2017-06-06 22:31:47 +0200

    Apps configuration based on enviroment variables

--- taskplus/apps/rest/app.py
+++ taskplus/apps/rest/app.py
@@ -1,13 +1,22 @@
+import os
 from flask import Flask
 
 from taskplus.apps.rest import routes
-from taskplus.apps.rest.settings import DevConfig
+from taskplus.apps.rest.settings import ProdConfig, DevConfig, TestConfig
 from taskplus.apps.rest.database import create_db
 from taskplus.apps.rest.auth import login_manager, user_loader, request_loader,\
                                     unauthorized_handler
 
 
-def create_app(config_object=DevConfig):
+if os.environ.get('TESTING'):
+    config = TestConfig
+elif os.environ.get('PRODUCTION'):
+    config = ProdConfig
+else:
+    config = DevConfig
+
+
+def create_app(config_object=config):
     app = Flask(__name__)
     app.config.from_object(config_object)
     app.register_blueprint(routes.blueprint)
